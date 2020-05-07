8422baab0460cdb89cfb1f4c7f6787ea46713e1f
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-07 21:01:50 +0200

    push intermedia

--- main.js
+++ main.js
@@ -2,3 +2,13 @@
 // Con jQuery, scrivere un numero random tra 0 e 10 dentro ognuno.
 // BONUS 1: generare la griglia con jQuery
 // BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+
+// 1 soluzione
+
+// var quadrato = $('.quadrato');
+// var numero_random = 0;
+//
+// for(var i=0; i < quadrato.length; i++){
+//     numero_random = Math.floor(Math.random() * 11);
+//     $(quadrato[i]).text(numero_random);
+// }
